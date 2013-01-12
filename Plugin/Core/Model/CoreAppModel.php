16a92ed0598ba653df4fc80d16bb170efb65605c
frankfoerster <github@frankfoerster.com>
2013-01-12 19:44:41 +0100

    add CoreAppModel

---
+++ Plugin/Core/Model/CoreAppModel.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Wasabi wide base model
+ *
+ * PHP 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the below copyright notice.
+ *
+ * @copyright     Copyright 2013, Frank Förster (http://frankfoerster.com)
+ * @link          http://github.com/frankfoerster/wasabi
+ * @package       Wasabi
+ * @subpackage    Wasabi.Plugin.Core.Model
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('AppModel', 'Model');
+
+class CoreAppModel extends AppModel {
+
+	public $actsAs = array(
+		'Containable'
+	);
+
+	public $rescursive = -1;
+
+}
