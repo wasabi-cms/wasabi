2a726cca92c6fac0b27ef7251bec121185d1393d
frankfoerster <github@frankfoerster.com>
2013-01-12 19:48:34 +0100

    add User model

---
+++ Plugin/Core/Model/User.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * PHP 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the below copyright notice.
+ *
+ * @copyright     Copyright 2013, Frank Förster (http://frankfoerster.com)
+ * @link          http://github.com/frankfoerster/wasabi
+ * @package       Wasabi
+ * @subpackage    Wasabi.Plugin.Core.Model
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('CoreAppModel', 'Core.Model');
+
+class User extends CoreAppModel {
+
+	public function getAll($options = array()) {
+		return $this->find('all', $options);
+	}
+
+}
