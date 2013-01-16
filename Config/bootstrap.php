e8b6e7437e3b2b59993ce890827f50d2320cab85
frankfoerster <github@frankfoerster.com>
2013-01-16 10:15:29 +0100

    enable Migrations plugin

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -143,10 +143,10 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 
-CakePlugin::load('Core', array(
-	'bootstrap' => true,
-	'routes' => true
-));
+CakePlugin::load('Core', array('bootstrap' => true, 'routes' => false));
+CakePlugin::load('Migrations');
+
+
 
 /**
  * You can attach event listeners to the request lifecyle as Dispatcher Filter . By Default CakePHP bundles two filters:
