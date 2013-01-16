b49856718a98e4aa4f96edb7b8bd2455f8627be9
frankfoerster <github@frankfoerster.com>
2013-01-16 15:01:33 +0100

    load plugin routes via new event manager

--- Config/routes.php
+++ Config/routes.php
@@ -25,17 +25,19 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	#Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	#Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
  * Load all plugin routes.  See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
-	CakePlugin::routes();
+	#CakePlugin::routes();
+
+	WasabiEventManager::trigger(new stdClass(), 'Plugin.Routes.load');
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
