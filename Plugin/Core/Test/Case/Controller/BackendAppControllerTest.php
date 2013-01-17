cde1b564e3168a4d0e080eab311cabb1a838e066
frankfoerster <github@frankfoerster.com>
2013-01-17 14:25:27 +0100

    update BackendAppController test cases

--- Plugin/Core/Test/Case/Controller/BackendAppControllerTest.php
+++ Plugin/Core/Test/Case/Controller/BackendAppControllerTest.php
@@ -13,18 +13,85 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
+App::uses('AuthenticatorComponent', 'Core.Controller/Component');
 App::uses('BackendAppController', 'Core.Controller');
+App::uses('Configure', 'Core');
 App::uses('ControllerTestCase', 'TestSuite');
 
+class BackendAppTestController extends BackendAppController {
+
+	public $redirectUrl;
+	public $checkPermissionsCalled = false;
+	public $setupBackendCalled = false;
+
+	public function checkPermisions() {
+		$this->_checkPermissions();
+	}
+
+	public function setupBackend() {
+		$this->_setupBackend();
+	}
+
+	public function loadBackendMenu() {
+		$this->_loadBackendMenu();
+	}
+
+	public function redirect($url) {
+		$this->redirectUrl = $url;
+		return false;
+	}
+
+	protected function _checkPermissions() {
+		$this->checkPermissionsCalled = true;
+		parent::_checkPermissions();
+	}
+
+	protected function _setupBackend() {
+		$this->setupBackendCalled = true;
+		parent::_setupBackend();
+	}
+
+}
+
+class AuthenticatorTest1Component extends AuthenticatorComponent {
+
+	public function __construct(ComponentCollection $collection) {
+		return parent::__construct($collection, array(
+			'model' => 'Foo',
+			'sessionKey' => 'wasabi',
+			'cookieKey' => 'wasabi'
+		));
+	}
+
+	public function get() {
+		return array();
+	}
+
+}
+
+class AuthenticatorTest2Component extends AuthenticatorTest1Component {
+
+	public function get() {
+		return array(
+			'User' => array(
+				'id' => 1,
+				'username' => 'admin'
+			)
+		);
+	}
+
+}
+
 /**
- * @property BackendAppController $BackendAppController
+ * @property BackendAppTestController $BackendAppController
  */
 
 class BackendAppControllerTest extends ControllerTestCase {
 
 	public function setUp() {
-		$this->BackendAppController = new BackendAppController();
+		$this->BackendAppController = new BackendAppTestController();
 		$this->BackendAppController->constructClasses();
+		$this->BackendAppController->Components->init($this->BackendAppController);
 	}
 
 	public function testRequiredComponentsAreLoaded() {
@@ -38,8 +105,77 @@ class BackendAppControllerTest extends ControllerTestCase {
 		$this->assertTrue(in_array('Session', $this->BackendAppController->helpers));
 	}
 
+	public function testBeforeFilter() {
+		$this->BackendAppController->request->params = Router::parse('/backend/users');
+		$this->BackendAppController->request->url = 'backend/users';
+		$this->BackendAppController->Authenticator = new AuthenticatorTest1Component($this->BackendAppController->Components);
+		$this->BackendAppController->beforeFilter();
+
+		$this->assertTrue($this->BackendAppController->checkPermissionsCalled);
+		$this->assertTrue($this->BackendAppController->setupBackendCalled);
+	}
+
+	public function testCheckPermissions() {
+		$this->BackendAppController->request->params = Router::parse('/backend/users');
+		$this->BackendAppController->request->url = 'backend/users';
+		$this->BackendAppController->Authenticator = new AuthenticatorTest1Component($this->BackendAppController->Components);
+
+		$this->BackendAppController->checkPermisions();
+		$this->assertTrue($this->BackendAppController->Session->check('login_referer'));
+
+		$expected = 'backend/users';
+		$result = $this->BackendAppController->Session->read('login_referer');
+		$this->assertEqual($expected, $result);
+
+		$expected = array(
+			'plugin' => 'core',
+			'controller' => 'users',
+			'action' => 'login'
+		);
+		$result = $this->BackendAppController->redirectUrl;
+		$this->assertEqual($expected, $result);
+
+		$this->BackendAppController->Session->delete('login_referer');
+		$this->BackendAppController->Authenticator = new AuthenticatorTest2Component($this->BackendAppController->Components);
+
+		$this->BackendAppController->checkPermisions();
+		$this->assertFalse($this->BackendAppController->Session->check('login_referer'));
+
+		$this->BackendAppController->request->params = Router::parse('/backend/login');
+		$this->BackendAppController->request->url = 'backend/login';
+		$this->BackendAppController->Authenticator = new AuthenticatorTest1Component($this->BackendAppController->Components);
+
+		$this->BackendAppController->checkPermisions();
+		$this->assertFalse($this->BackendAppController->Session->check('login_referer'));
+	}
+
+	public function testSetupBackend() {
+		$this->BackendAppController->request->params = Router::parse('/backend/users');
+		$this->BackendAppController->request->url = 'backend/users';
+		$this->BackendAppController->setupBackend();
+		$expected = 'Core.default';
+		$result = $this->BackendAppController->layout;
+		$this->assertEqual($expected, $result);
+
+		$this->assertTrue(isset($this->BackendAppController->viewVars['backend_prefix']));
+		$expected = Configure::read('Wasabi.backend_prefix');
+		$result = $this->BackendAppController->viewVars['backend_prefix'];
+		$this->assertEqual($expected, $result);
+	}
+
+	public function testLoadBackendMenu() {
+		$this->BackendAppController->request->params = Router::parse('/backend/users');
+		$this->BackendAppController->request->url = 'backend/users';
+		$this->BackendAppController->loadBackendMenu();
+		$this->assertTrue(isset($this->BackendAppController->viewVars['backend_menu_for_layout']));
+		$this->assertTrue(isset($this->BackendAppController->viewVars['backend_menu_for_layout']['primary']));
+		$this->assertTrue(isset($this->BackendAppController->viewVars['backend_menu_for_layout']['secondary']));
+	}
+
 	public function tearDown() {
-		unset($this->BackendApp);
+		$this->BackendAppController->Session->delete('wasabi');
+		$this->BackendAppController->Session->delete('login_referer');
+		unset($this->BackendAppController);
 		ClassRegistry::flush();
 	}
 
