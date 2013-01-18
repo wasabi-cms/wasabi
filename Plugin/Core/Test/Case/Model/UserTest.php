28252bf19fb729f8da4f51661f146e2ff254a749
frankfoerster <github@frankfoerster.com>
2013-01-18 19:16:54 +0100

    coding style

--- Plugin/Core/Test/Case/Model/UserTest.php
+++ Plugin/Core/Test/Case/Model/UserTest.php
@@ -43,8 +43,30 @@ class UserTest extends CakeTestCase {
 	public function testFindAll() {
 		$result = $this->User->findAll();
 		$expected = array(
-			array('User' => array('id' => 1, 'group_id' => 1, 'language_id' => 1, 'username' => 'admin', 'password' => '$2a$10$XgE0KcjO4WNIXZIPk.6dQ.ZXTCf5pxVxdx9SIh5p5JMe9iSd8ceIO', 'active' => 1, 'created' => '2013-01-12 14:00:00', 'modified' => '2013-01-12 14:00:00')),
-			array('User' => array('id' => 2, 'group_id' => 1, 'language_id' => 1, 'username' => 'test', 'password' => '$2a$10$i4q2qRWt5dX5O/C.Nldq5evjpY3MNMlG3K4BrxsXH7zBZmxqwzAUO', 'active' => 0, 'created' => '2013-01-12 15:00:00', 'modified' => '2013-01-12 15:00:00'))
+			array(
+				'User' => array(
+					'id' => 1,
+					'group_id' => 1,
+					'language_id' => 1,
+					'username' => 'admin',
+					'password' => '$2a$10$XgE0KcjO4WNIXZIPk.6dQ.ZXTCf5pxVxdx9SIh5p5JMe9iSd8ceIO',
+					'active' => 1,
+					'created' => '2013-01-12 14:00:00',
+					'modified' => '2013-01-12 14:00:00'
+				)
+			),
+			array(
+				'User' => array(
+					'id' => 2,
+					'group_id' => 1,
+					'language_id' => 1,
+					'username' => 'test',
+					'password' => '$2a$10$i4q2qRWt5dX5O/C.Nldq5evjpY3MNMlG3K4BrxsXH7zBZmxqwzAUO',
+					'active' => 0,
+					'created' => '2013-01-12 15:00:00',
+					'modified' => '2013-01-12 15:00:00'
+				)
+			)
 		);
 		$this->assertEqual($expected, $result);
 
