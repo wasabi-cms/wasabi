52e43ece4271f2a5996b849ac141679a2965c36c
frankfoerster <github@frankfoerster.com>
2013-03-03 00:08:14 +0100

    add Languages add/edit/delete actions

--- Plugin/Core/Controller/LanguagesController.php
+++ Plugin/Core/Controller/LanguagesController.php
@@ -17,6 +17,7 @@ App::uses('BackendAppController', 'Core.Controller');
 
 /**
  * @property Language $Language
+ * @property array $data
  */
 
 class LanguagesController extends BackendAppController {
@@ -40,4 +41,74 @@ class LanguagesController extends BackendAppController {
 		$this->set('languages', $languages);
 	}
 
+	/**
+	 * Add action
+	 * GET | POST
+	 *
+	 * @return void
+	 */
+	public function add() {
+		$this->set('title_for_layout', __d('core', 'Add a new Language'));
+		if ($this->request->is('post') && !empty($this->data)) {
+			if ($this->Language->save($this->data)) {
+				$this->Session->setFlash(__d('core', 'The language <strong>%s</strong> has been added.', array($this->data['Language']['name'])), 'default', array('class' => 'success'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash($this->formErrorMessage, 'default', array('class' => 'error'));
+			}
+		}
+	}
+
+	/**
+	 * Edit action
+	 * GET | POST
+	 *
+	 * @param null|integer $id
+	 * @return void
+	 */
+	public function edit($id = null) {
+		if ($id === null) {
+			$this->Session->setFlash($this->invalidRequestMessage, 'default', array('class' => 'error'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('title_for_layout', __d('core', 'Edit Language'));
+		if (!$this->request->is('post') && empty($this->data)) {
+			$this->request->data = $this->Language->findById($id);
+		} else {
+			if ($this->Language->save($this->data)) {
+				$this->Session->setFlash(__d('core', 'The language <strong>%s</strong> has been updated successfully.', array($this->data['Language']['name'])), 'default', array('class' => 'success'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash($this->formErrorMessage, 'default', array('class' => 'error'));
+			}
+		}
+		$this->render('add');
+	}
+
+	/**
+	 * Delete action
+	 * POST
+	 *
+	 * @param null|integer $id
+	 * @return void
+	 * @throws MethodNotAllowedException
+	 */
+	public function delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+
+		if ($id === null || !$this->Language->canBeDeleted($id)) {
+			$this->Session->setFlash($this->invalidRequestMessage, 'default', array('class' => 'error'));
+			$this->redirect(array('action' => 'index'));
+		}
+
+		if ($this->Language->delete($id)) {
+			$this->Session->setFlash(__d('core', 'The language has been deleted.'), 'default', array('class' => 'success'));
+			$this->redirect(array('action' => 'index'));
+		}
+
+		$this->Session->setFlash(__d('core', 'The language has not been deleted.'), 'default', array('class' => 'error'));
+		$this->redirect(array('action' => 'index'));
+	}
 }
