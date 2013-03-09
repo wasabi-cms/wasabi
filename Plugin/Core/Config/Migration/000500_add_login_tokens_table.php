bdc4ff4bc4c87e6586eab56026db8ab10c0a69e6
frankfoerster <github@frankfoerster.com>
2013-03-09 14:40:13 +0100

    add comments and CakeMigration requirement

--- Plugin/Core/Config/Migration/000500_add_login_tokens_table.php
+++ Plugin/Core/Config/Migration/000500_add_login_tokens_table.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ *
+ * PHP 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the below copyright notice.
+ *
+ * @copyright     Copyright 2013, Frank Förster (http://frankfoerster.com)
+ * @link          http://github.com/frankfoerster/wasabi
+ * @package       Wasabi
+ * @subpackage    Wasabi.Plugin.Core.Config.Migration
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('CakeMigration', 'Migrations.Lib');
+
 class AddLoginTokensTable extends CakeMigration {
 
 /**
