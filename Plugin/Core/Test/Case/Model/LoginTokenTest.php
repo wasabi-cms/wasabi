2f6adea0f666231860532c3aa2b7bfd64f248488
frankfoerster <github@frankfoerster.com>
2013-03-24 15:57:57 +0100

    add missing tests

--- Plugin/Core/Test/Case/Model/LoginTokenTest.php
+++ Plugin/Core/Test/Case/Model/LoginTokenTest.php
@@ -22,20 +22,50 @@ App::uses('ClassRegistry', 'Utility');
 
 class LoginTokenTest extends CakeTestCase {
 
+	public $fixtures = array('plugin.core.login_token', 'plugin.core.user');
+
 	public function setUp() {
 		$this->LoginToken = ClassRegistry::init('Core.LoginToken');
 
 		parent::setUp();
 	}
 
-	public function testBelongsToUser() {
-		$this->assertTrue(array_key_exists('User', $this->LoginToken->belongsTo));
-	}
-
 	public function tearDown() {
 		unset($this->LoginToken);
 
 		parent::tearDown();
+	}
+
+	public function testBelongsToUser() {
+		$this->assertTrue(array_key_exists('User', $this->LoginToken->belongsTo));
+	}
+
+	public function testFindActiveToken() {
+		$this->assertNotEmpty($this->LoginToken->findActiveToken('i_am_a_very_secret_token'));
+	}
+
+	public function testAdd() {
+		$token_count = $this->LoginToken->find('count');
+
+		$this->assertFalse($this->LoginToken->hasAny(array('user_id' => 99)));
+		$this->assertNotEmpty($this->LoginToken->add(99, 'yoyo', '2 weeks'));
+		$this->assertEqual(($token_count + 1), $this->LoginToken->find('count'));
+		$this->assertTrue($this->LoginToken->hasAny(array('user_id' => 99)));
+	}
+
+	public function testAlreadyExists() {
+		$this->assertTrue($this->LoginToken->alreadyExists('i_am_a_very_secret_token'));
+		$this->assertFalse($this->LoginToken->alreadyExists('new_token'));
+	}
+
+	public function testDeleteExpiredTokens() {
+		$this->assertEqual(1, $this->LoginToken->find('count'));
+
+		$this->LoginToken->id = 1;
+		$this->LoginToken->saveField('expires', date('Y-m-d H:i:s', strtotime('-2 weeks')));
+
+		$this->assertTrue($this->LoginToken->deleteExpiredTokens());
+		$this->assertEqual(0, $this->LoginToken->find('count'));
 	}
 
 }
