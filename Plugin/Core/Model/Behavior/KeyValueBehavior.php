6bbfe0f00ea988566bff53f52a0ca8eb2596fffa
frankfoerster <github@frankfoerster.com>
2013-08-31 22:11:32 +0200

    add KeyValueBehavior

---
+++ Plugin/Core/Model/Behavior/KeyValueBehavior.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ *
+ * PHP 5
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the below copyright notice.
+ *
+ * @copyright     Copyright 2013, Frank Förster (http://frankfoerster.com)
+ * @link          http://github.com/frankfoerster/wasabi
+ * @package       Wasabi
+ * @subpackage    Wasabi.Plugin.Core.Model.Behavior
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('Hash', 'Utility');
+App::uses('ModelBehavior', 'Model');
+
+class KeyValueBehavior extends ModelBehavior {
+
+	/**
+	 * Map custom find types to each model the behavior is
+	 * attached to.
+	 *
+	 * @var array
+	 */
+	public $mapMethods = array('/\b_findKeyValues\b/' => 'findKeyValues');
+
+	/**
+	 * Holds the default behavior settings for each model.
+	 *
+	 * @var array
+	 */
+	protected $_settings = array();
+
+	/**
+	 * Defines default behavior settings.
+	 *
+	 * @var array
+	 */
+	protected $_defaults = array(
+		'scope' => 'App',
+		'fields' => array(
+			'key' => 'key',
+			'value' => 'value'
+		)
+	);
+
+	/**
+	 * Initiate behavior for the model using specified settings.
+	 *
+	 * Options:
+	 *  - scope: This string defines the current scope namespace
+	 *  - uniqueKeys: If this is true it deletes all pre-existing entries for the key on save.
+	 *  - fields: Can be used to override the default mapping of 'key' and 'value' fields.
+	 *
+	 * @param Model $model
+	 * @param array $options
+	 */
+	public function setup(Model $model, $options = array()) {
+		$this->_settings[$model->alias] = Hash::merge($this->_defaults, $options);
+		$model->findMethods['keyValues'] = true;
+	}
+
+	/**
+	 * Save key value pairs.
+	 *
+	 * @param Model $model
+	 * @param array $data the request->data array
+	 * @return boolean|array
+	 */
+	public function saveKeyValues(Model $model, $data) {
+		/**
+		 * @var string $scope
+		 * @var array $fields
+		 */
+		extract($this->_settings[$model->alias]);
+		$keys = array_keys($data[$model->alias]);
+		$rawData = $data;
+
+		if ((in_array($fields['key'], $keys) &&	in_array($fields['value'], $keys)) ||
+			!(isset($rawData[$model->alias]) && !empty($rawData[$model->alias]))
+		) {
+			return true;
+		}
+
+		$model->create($data);
+		if (!$model->validates()) {
+			return false;
+		}
+
+		$mapping = $this->_getMapping($model);
+
+		$dataToSave = array();
+
+		foreach ($rawData[$model->alias] as $key => $value) {
+			if ($model->hasField($key)) {
+				continue;
+			}
+
+			$data = array(
+				$model->alias => array(
+					'scope' => $scope,
+					$fields['key'] => $key,
+					$fields['value'] => $value
+				)
+			);
+
+			$mappingKey = $scope . '.' . $key;
+			if (isset($mapping[$mappingKey])) {
+				$data[$model->alias]['id'] = $mapping[$mappingKey];
+			}
+
+			$dataToSave[] = $data;
+		}
+
+		return $model->saveAll($dataToSave, array('validate' => false));
+	}
+
+	/**
+	 * Custom find type 'keyValues'.
+	 *
+	 * @param Model $model
+	 * @param $functionCall
+	 * @param string $state
+	 * @param array $query
+	 * @param array $results
+	 * @return array
+	 */
+	public function findKeyValues(Model $model, $functionCall, $state, $query, $results = array()) {
+		/**
+		 * @var string $scope
+		 * @var array $fields
+		 */
+		extract($this->_settings[$model->alias]);
+		if ($state === 'before') {
+			$query['conditions'][$model->alias . '.scope'] = $scope;
+			return $query;
+		}
+
+		$rawResults = $results;
+		$results = array(
+			$model->alias => array()
+		);
+
+		foreach ($rawResults as $result) {
+			if (!isset($result[$model->alias][$fields['key']]) || !isset($result[$model->alias][$fields['value']])) {
+				continue;
+			}
+			$key = $result[$model->alias][$fields['key']];
+			$value = $result[$model->alias][$fields['value']];
+			$results[$model->alias][$key] = $value;
+		}
+
+		return $results;
+	}
+
+	/**
+	 * Returns an array of existing keys and
+	 * their corresponding ids.
+	 *
+	 * @param Model $model
+	 * @return array
+	 */
+	protected function _getMapping(Model $model) {
+		/**
+		 * @var string $scope
+		 * @var array $fields
+		 */
+		extract($this->_settings[$model->alias]);
+		$results = $model->find('all');
+		$mapping = array();
+
+		foreach ($results as $result) {
+			$key = $scope . '.' . $result[$model->alias][$fields['key']];
+			$mapping[$key] = $result[$model->alias]['id'];
+		}
+
+		return $mapping;
+	}
+}
